cac7c41a65333f1ccb16525cfb8c807709b84885
nwaamadioha <udokwu.temple960@gmail.com>
2022-05-06 19:35:28 +0100

    Frontend using  react

---
+++ src/container/Skills/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./Skills.scss";
+
+const Skill = () => {
+  return (
+    <div>Skill</div>
+  )
+}
+
+export default Skill
---
+++ src/container/Work/Work.jsx
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from 'react';
+import {AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+
+import "./Work.scss";
+
+const Work = () => {
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setanimateCard] = useState({ y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([])
+
+
+  useEffect(() => {
+
+    const query = "*[_type == 'works']";
+    client.fetch(query)
+      .then((data) => {
+        setWorks(data);
+        setFilterWork(data);
+      })
+  }, [])
+  
+
+  function handleWorkFilter(item) {
+  
+  }
+  return (
+    <>
+      <h2 className='head-text'>My Creative <span>Portfolio</span><br /> Section</h2>
+      <div className='app__work-filter'>
+        {['UI/UX', 'Web App', 'React JS', 'Node JS', 'Python Django', 'All'].map((item, index) => (
+          <div 
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter app__flex p-text ${activeFilter === item? 'item-active': ''}`}
+          >
+            {item}
+          </div>
+        ) )}
+      </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5}}
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work, index) => (
+          <div className='app__work-item app__flex' key={index}>
+            <div className='app__work-img app__flex'>
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </>
+  )
+}
+
+export default Work
